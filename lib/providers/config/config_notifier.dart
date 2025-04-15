99069568b3c2f56be9c15400b766d695b8021556
Martin E. Tau <taumartin@gmail.com>
2025-04-15 17:07:08 -0300

    Changed Configutarion conn. default values & removed PoMA auth related config.

--- lib/providers/config/config_notifier.dart
+++ lib/providers/config/config_notifier.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ConfigNotifier extends ChangeNotifier {
-  String _deviceHost = "1.0.0.1";
-  int _devicePort = 666;
-  String _deviceAuthUser = "lucas\n"; // FIXME: It should not end with "\n".
-  String _deviceAuthPass = "stream\n"; // FIXME: It should not end with "\n".
+  String _deviceHost = "192.168.20.27";
+  int _devicePort = 3333;
   int _deviceConnectionTimeout = 10;
-  bool _deviceConnectionUseCredentials = true;
 
   String get deviceHost => _deviceHost;
 
   int get devicePort => _devicePort;
 
-  String get deviceAuthUser => _deviceAuthUser;
-
-  String get deviceAuthPass => _deviceAuthPass;
-
   int get deviceConnectionTimeout => _deviceConnectionTimeout;
-
-  bool get deviceConnectionUseCredentials => _deviceConnectionUseCredentials;
 
   void updateSettings({
     String? deviceHost,
@@ -26,7 +17,6 @@ class ConfigNotifier extends ChangeNotifier {
     String? deviceAuthUser,
     String? deviceAuthPass,
     int? deviceConnectionTimeout,
-    bool? deviceConnectionUseCredentials,
   }) {
     if (deviceHost != null) {
       _deviceHost = deviceHost;
@@ -34,17 +24,8 @@ class ConfigNotifier extends ChangeNotifier {
     if (devicePort != null) {
       _devicePort = devicePort;
     }
-    if (deviceAuthUser != null) {
-      _deviceAuthUser = deviceAuthUser;
-    }
-    if (deviceAuthPass != null) {
-      _deviceAuthPass = deviceAuthPass;
-    }
     if (deviceConnectionTimeout != null) {
       _deviceConnectionTimeout = deviceConnectionTimeout;
-    }
-    if (deviceConnectionUseCredentials != null) {
-      _deviceConnectionUseCredentials = deviceConnectionUseCredentials;
     }
     notifyListeners();
   }
